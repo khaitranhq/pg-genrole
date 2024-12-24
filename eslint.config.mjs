75715606590f5f1b02225a1f8b41e63389f83037
Khai Tran <khaitran.lewis@gmail.com>
2024-12-24 16:57:42 +0700

    ✨ Add Prettier and ESLint configuration files to enforce code style and quality standards. This improves code consistency and helps catch potential issues early in the development process.

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn'
+    }
+  }
+);
